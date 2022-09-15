d4f6fa8dd00824cd490895c69d3af77ad3789460
Xuzhengz <2350122040@qq.com>
2022-09-15 21:20:21 +0800

    2022-09-15-定时器

--- src/main/java/com/xzz/chapter07/KeyByProcessFunctionTest.java
+++ src/main/java/com/xzz/chapter07/KeyByProcessFunctionTest.java
@@ -3,6 +3,7 @@ package com.xzz.chapter07;
 import com.xzz.chapter06.UserBehavior;
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -26,13 +27,13 @@ public class KeyByProcessFunctionTest {
 
         URL resourceAsStream = ClassLoader.getSystemClassLoader().getResource("UserBehavior.csv");
 
-//        DataStreamSource<String> inputStream = env.socketTextStream("hadoop102", 7777);
+        DataStreamSource<String> inputStream = env.socketTextStream("hadoop102", 7777);
 
-        DataStreamSource<String> inputStream = env.readTextFile(resourceAsStream.getPath());
+//        DataStreamSource<String> inputStream = env.readTextFile(resourceAsStream.getPath());
 
         SingleOutputStreamOperator<UserBehavior> userBehaviorStream = inputStream.map(line -> {
             String[] fields = line.split(",");
-            return new UserBehavior(new Long(fields[0]), new Long(fields[1]), new Long(fields[2]), fields[3], new Long(fields[4]));
+            return new UserBehavior(new Long(fields[0]), new Long(fields[1]), new Long(fields[2]), fields[3], System.currentTimeMillis());
         }).assignTimestampsAndWatermarks(WatermarkStrategy.<UserBehavior>forBoundedOutOfOrderness(Duration.ZERO).withTimestampAssigner(new SerializableTimestampAssigner<UserBehavior>() {
             @Override
             public long extractTimestamp(UserBehavior userBehavior, long l) {
@@ -40,20 +41,22 @@ public class KeyByProcessFunctionTest {
             }
         }));
 
-        userBehaviorStream.keyBy(data -> data.getUserid())
-                .process(new KeyedProcessFunction<Long, UserBehavior, String>() {
+        userBehaviorStream.keyBy(data -> true)
+                .process(new KeyedProcessFunction<Boolean, UserBehavior, String>() {
                     @Override
-                    public void processElement(UserBehavior userBehavior, Context context, Collector<String> collector) throws Exception {
-//                        collector.collect("当前数据的事件时间：" + userBehavior.getTimestamp());
-
-                        //注册10S后的定时器
-                        context.timerService().registerProcessingTimeTimer(context.timerService().currentProcessingTime() + 10 * 1000L);
+                    public void processElement(UserBehavior value, Context ctx, Collector<String> out) throws Exception {
+                        Long currTs = ctx.timerService().currentProcessingTime();
+                        out.collect("数据到达，到达时间：" + new Timestamp(currTs));
+                        // 注册一个 10 秒后的定时器
+                        ctx.timerService().registerProcessingTimeTimer(currTs + 10 * 1000L);
                     }
+
                     @Override
                     public void onTimer(long timestamp, OnTimerContext ctx, Collector<String> out) throws Exception {
-                        out.collect("定时器触发时间" + timestamp);
+                        out.collect(ctx.getCurrentKey().toString());
                     }
-                }).print("timeserver：");
+                })
+                .print();
 
 
         env.execute();
