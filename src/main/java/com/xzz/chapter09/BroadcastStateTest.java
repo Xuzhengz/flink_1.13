c97b249989daed42ca108cd28683306f4ed629d6
Xuzhengz <2350122040@qq.com>
2022-09-23 13:29:39 +0800

    2022-09-23-状态持久化和状态后端

--- src/main/java/com/xzz/chapter09/BroadcastStateTest.java
+++ src/main/java/com/xzz/chapter09/BroadcastStateTest.java
@@ -14,6 +14,7 @@ import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.co.KeyedBroadcastProcessFunction;
 import org.apache.flink.util.Collector;
+
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -50,20 +51,18 @@ public class BroadcastStateTest {
                 new Action("wentao", "order")
         );
 
-        DataStreamSource<Pattern> patterntream = env.fromElements(
+        DataStreamSource<Pattern> patternStream = env.fromElements(
                 new Pattern("login", "pay"),
                 new Pattern("login", "order")
         );
 
-        //定义广播状态描述器
-
-        MapStateDescriptor<Void, Pattern> pattern = new MapStateDescriptor<>("pattern", Types.VOID, Types.POJO(Pattern.class));
-
-        BroadcastStream<Pattern> broadcast = patterntream.broadcast(pattern);
+        // 定义广播状态的描述器，创建广播流
+        MapStateDescriptor<Void, Pattern> bcStateDescriptor = new MapStateDescriptor<>("patterns", Void.class,Pattern.class);
+            BroadcastStream<Pattern> bcPatterns = patternStream.broadcast(bcStateDescriptor);
 
         SingleOutputStreamOperator<Tuple2<String, Pattern>> matches = actionStream.keyBy(data -> data.getUserId())
-                .connect(broadcast)
-                .process(new PatternDetector());
+                .connect(bcPatterns)
+                .process(new PatternEvaluator());
 
         matches.print();
 
@@ -150,36 +149,36 @@ public class BroadcastStateTest {
         }
     }
 
-    private class PatternDetector extends KeyedBroadcastProcessFunction<String, Action, Pattern, Tuple2<String, Pattern>> {
-        // 定义一个键值状态，保存用户上一次行为
-
-        ValueState<String> valueState;
+    public static class PatternEvaluator extends KeyedBroadcastProcessFunction<String, Action, Pattern, Tuple2<String, Pattern>> {
+        // 定义一个值状态，保存上一次用户行为
+        ValueState<String> prevActionState;
 
         @Override
-        public void open(Configuration parameters) throws Exception {
-            valueState = getRuntimeContext().getState(new ValueStateDescriptor<String>("value", String.class));
+        public void open(Configuration conf) {
+            prevActionState = getRuntimeContext().getState(new ValueStateDescriptor<>("lastAction", Types.STRING));
         }
 
         @Override
-        public void processElement(Action action, ReadOnlyContext readOnlyContext, Collector<Tuple2<String, Pattern>> collector) throws Exception {
-            ReadOnlyBroadcastState<Void, Pattern> patternBroadcastState = readOnlyContext.getBroadcastState(new MapStateDescriptor<>("pattern", Types.VOID, Types.POJO(Pattern.class)));
-            Pattern pattern = patternBroadcastState.get(null);
-            String lastValue = valueState.value();
+        public void processBroadcastElement(Pattern pattern, Context ctx, Collector<Tuple2<String, Pattern>> out) throws Exception {
+            BroadcastState<Void, Pattern> bcState = ctx.getBroadcastState(new MapStateDescriptor<>("patterns",  Void.class,Pattern.class));
+            // 将广播状态更新为当前的 pattern
+            bcState.put(null, pattern);
+        }
 
-            if (pattern != null && lastValue != null) {
-                if (pattern.getAction1().equals(lastValue) && pattern.getAction2().equals(action.getAction())){
-                    collector.collect(new Tuple2<>(readOnlyContext.getCurrentKey(),pattern));
+        @Override
+        public void processElement(Action action, ReadOnlyContext ctx, Collector<Tuple2<String, Pattern>> out) throws Exception {
+            Pattern pattern = ctx.getBroadcastState(new MapStateDescriptor<>("patterns", Void.class,Pattern.class )).get(null);
+            String prevAction = prevActionState.value();
+            if (pattern != null && prevAction != null) {
+                // 如果前后两次行为都符合模式定义，输出一组匹配
+                if (pattern.action1.equals(prevAction) &&
+                        pattern.action2.equals(action.action)) {
+                    out.collect(new Tuple2<>(ctx.getCurrentKey(), pattern));
                 }
-
-                valueState.update(action.getAction());
-
             }
-        }
-
-        @Override
-        public void processBroadcastElement(Pattern pattern, Context context, Collector<Tuple2<String, Pattern>> collector) throws Exception {
-            BroadcastState<Void, Pattern> patternBroadcastState = context.getBroadcastState(new MapStateDescriptor<>("pattern", Types.VOID, Types.POJO(Pattern.class)));
-            patternBroadcastState.put(null, pattern);
+            // 更新状态
+            prevActionState.update(action.action);
         }
     }
+
 }
